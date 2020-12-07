070593b5f624b6798c204e58f00bd55f91e2350f
kantinant321 <62507168+kantinant321@users.noreply.github.com>
2020-12-07 18:16:15 +0700

    n

--- js/index.js
+++ js/index.js
@@ -29,11 +29,11 @@ function m() {
 
 $(function () {
   var db = firebase.firestore();
-  db.collection("pic").get().then((querySnapshot) => {
+  db.collection("movie").get().then((querySnapshot) => {
          
       querySnapshot.forEach((doc) => {
               var num = `${doc.data().listN}`
-              var card = ` <img src="${doc.data().URL}" width="375" height="195" id="m1">
+              var card = ` <img src="${doc.data().posterURL}" width="375" height="550" id="m1">
               `;
               $("#m" + num).append(card);
           });
